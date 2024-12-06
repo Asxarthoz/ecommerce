f9fb4d15c32cfe56bcf1c3f967a8a3183e3d8437
Asxarthoz <zidandicky2006@gmail.com>
2024-12-06 08:40:12 +0700

    create art.c
    file c untuk UI daii produk

---
+++ art.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void LaptopGaming()
+{
+    printf("   ._________________.\n");
+    printf("   |.---------------.|\n");
+    printf("   ||               ||\n");
+    printf("   ||   -._ .-.     ||\n");
+    printf("   ||   -._| | |    ||\n");
+    printf("   ||   -._| | |    ||\n");
+    printf("   ||   -._|.-.|    ||\n");
+    printf("   ||_______________||\n");
+    printf("   /.-.-.-.-.-.-.-.-.\\\n");
+    printf("  /.-.-.-.-.-.-.-.-.-.\\\n");
+    printf(" /.-.-.-.-.-.-.-.-.-.-.\\\n");
+    printf("/______/__________\\___o_\\\n");
+    printf("\\_______________________/\n");
+    
+}
+
+void Kemeja()
+{
+    printf("   ___ ___\n");
+    printf(" /| |/|\\| |\\\n");
+    printf("/_|   |.` |_\\\n");
+    printf("  |   |.  |\n");
+    printf("  |   |.  |\n");
+    printf("  |___|.__|\n");
+}
+
+void BerasPremium()
+{
+    printf("-----------------\n");
+    printf("-----------------\n");
+    printf("-               -\n");
+    printf("-  -- BERAS --  -\n");
+    printf("-               -\n");
+    printf("-    PREMIUM    -\n");
+    printf("-               -\n");
+    printf("-  -         -  -\n");
+    printf("-    -------    -\n");
+    printf("-----------------\n");
+
+}
+
+void MejaKerja()
+{
+    printf("                      ,@\n");
+    printf("                     ,@@\n");
+    printf("        ______________)(______________\n");
+    printf("       /             (__)             \\\n");
+    printf("      /________________________________\\\n");
+    printf("      [________________________________]\n");
+    printf("         \\ \\  / /            \\ \\  / /\n");
+    printf("          \\ \\/ /              \\ \\/ /\n");
+    printf("          _\\/ /________________\\ \\/_\n");
+    printf("         [_/o/__________________\\o\\_]\n");
+    printf("          / /\\ \\              / /\\ \\\n");
+    printf("         / /  \\_\\            /_/  \\_\\ \n");
+
+}
+
+
+
+int main()
+{
+
+    LaptopGaming();
+    Kemeja();
+    BerasPremium();
+    MejaKerja();
+
+return 0;
+}
